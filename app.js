44dfaefe342873c0d479b3f42e71e098fd6818a2
idmit <dma.biz@yandex.by>
2024-01-04 15:42:49 +0300

    refactor(basic-2.5): change line-wrapping for html attributes

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ var createNewTaskElement=function(taskString){
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+		checkBox.className="input-checkbox";
     editInput.type="text";
     editInput.className="task-input";
 
